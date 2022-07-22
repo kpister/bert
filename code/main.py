1dfadd14fc3a8306bc934ad7b6ac93e0a1f53b00
Kaiser <kaiser@sapling.gg>
2022-07-22 12:45:38 -0700

    add model saving

--- code/main.py
+++ code/main.py
@@ -1,10 +1,11 @@
-from config import MAX_LEN, VOCAB_SIZE
-from preprocessing import load_dataloaders
-from model import BERTLM, ScheduledOptim
-from torch.optim import Adam
+import torch
 from torch import nn
+from torch.optim import Adam
 import pytorch_lightning
 from pytorch_lightning.lite import LightningLite
+from model import BERTLM, ScheduledOptim
+from config import MAX_LEN, VOCAB_SIZE
+from preprocessing import load_dataloaders
 
 
 def cycle(dl):
@@ -32,31 +33,38 @@ class Lite(LightningLite):
 
         print("Training Start")
         model.train()
-        for epoch in range(num_epochs):
-            avg_loss = 0
-            for i, data in enumerate(train_data):
-                optimizer.zero_grad()
+        epoch = 0
+        avg_loss = 0
+        for i, data in enumerate(train_data):
+            if i % train_data_len == 0:
+                i = i % train_data_len
+                epoch += 1
+                if epoch >= num_epochs:
+                    break
+            optimizer.zero_grad()
 
-                nsp_output, mlm_output = model(
-                    data["bert_input"], data["segment_label"]
-                )
+            nsp_output, mlm_output = model(data["bert_input"], data["segment_label"])
 
-                nsp_loss = criterion(nsp_output, data["is_next"])
-                mlm_loss = criterion(
-                    mlm_output.reshape(data["bert_label"].size(0), VOCAB_SIZE, MAX_LEN),
-                    data["bert_label"],
-                )
-                loss = nsp_loss + mlm_loss
-                avg_loss += loss.item()
-                self.backward(loss)
-                optimizer.step_and_update_lr()
+            nsp_loss = criterion(nsp_output, data["is_next"])
+            mlm_loss = criterion(
+                mlm_output.reshape(data["bert_label"].size(0), VOCAB_SIZE, MAX_LEN),
+                data["bert_label"],
+            )
+            loss = nsp_loss + mlm_loss
+            avg_loss += loss.item()
+            self.backward(loss)
+            optimizer.step_and_update_lr()
 
+            if i % 10 == 0:
                 print(
                     f"Epoch: {epoch}.{i % train_data_len}/{train_data_len} "
                     f"| Loss: {avg_loss / (i+1):.3f} "
                     f"| NSP Loss: {nsp_loss.item():.3f} "
                     f"| MLM Loss {mlm_loss.item():.3f}"
                 )
+
+            if i % 1000 == 0:
+                torch.save(model, "models/m_{epoch}_{i}.pth")
 
 
 pytorch_lightning.seed_everything(0)
